332f241ae364e8f93db28b711a8e06d9b3f839c6
ryujaeun <ryu2253@naver.com>
2022-11-19 16:11:13 +0900

    os version setup update

--- serversetup.py
+++ serversetup.py
@@ -1,16 +1,21 @@
+import os
 def server_setup():
     try:
-        import os
-        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+        if os.name == 'posix':
+            os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-        import tensorflow as tf
-        os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-        gpus = tf.config.experimental.list_physical_devices('GPU')
-        if gpus:
-            try:
-                tf.config.experimental.set_memory_growth(gpus[0], True)
-            except RuntimeError as e:
-                print(e)
+            import tensorflow as tf
+            os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+            gpus = tf.config.experimental.list_physical_devices('GPU')
+            if gpus:
+                try:
+                    tf.config.experimental.set_memory_growth(gpus[0], True)
+                except RuntimeError as e:
+                    print(e)
+        elif os.name == 'nt':
+            pass
+        else:
+            raise ValueError("what's the os??")
     except:
         pass
         
